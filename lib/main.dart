6c9f41749daef86b261569c0630070ebb6e7faac
Aman Gupta <aman.kmcs@gmail.com>
2021-07-04 11:51:50 +0530

    Testing with kotlin
    Updated from 1.3.50 to 1.5.20

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+// import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   return runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  void audiofunc(int filenum) {
-    final player = AudioCache();
-    player.play('note$filenum.wav');
-  }
-
-  Expanded longbar({color: Color, required int filenum}) {
-    return Expanded(
-        child: TextButton(
-      onPressed: () {
-        audiofunc(filenum);
-      },
-      child: Card(
-        color: color,
-      ),
-    ));
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              longbar(color: Colors.blue, filenum: 1),
-              longbar(color: Colors.red, filenum: 2),
-              longbar(color: Colors.deepOrange, filenum: 3),
-              longbar(color: Colors.pinkAccent, filenum: 4),
-              longbar(color: Colors.brown, filenum: 5),
-              longbar(color: Colors.green, filenum: 6),
-              longbar(color: Colors.yellowAccent, filenum: 7),
-            ],
+      home: AudioApp(),
+    );
+  }
+}
+
+class AudioApp extends StatefulWidget {
+  const AudioApp({Key? key}) : super(key: key);
+  @override
+  _AudioAppState createState() => _AudioAppState();
+}
+
+class _AudioAppState extends State<AudioApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(150, 240, 200, 150),
+      body: SafeArea(
+        child: Center(
+          child: TextButton(
+            onPressed: () {},
+            child: Card(
+              child: Text('Hiiii'),
+            ),
           ),
         ),
       ),
